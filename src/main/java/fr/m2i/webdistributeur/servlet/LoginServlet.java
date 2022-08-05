b5acef7a1d7205a606e22ce0151279fc4fbc25d2
a rahni <rahniahmed@gmail.com>
2022-08-05 10:47:50 +0200

    add listener with use of context attribut

--- src/main/java/fr/m2i/webdistributeur/servlet/LoginServlet.java
+++ src/main/java/fr/m2i/webdistributeur/servlet/LoginServlet.java
@@ -1,5 +1,6 @@
 package fr.m2i.webdistributeur.servlet;
 
+import fr.m2i.webdistributeur.dao.UserDao;
 import fr.m2i.webdistributeur.entity.Role;
 import fr.m2i.webdistributeur.entity.User;
 import fr.m2i.webdistributeur.utils.UserDb;
@@ -39,20 +40,25 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //super.doPost(request, response);
-         // On récupère notre userDb depuis les attributs du servlet context (créé dans le LifecycleListener)
-        //UserDb userDb = (UserDb) this.getServletContext().getAttribute("userDb");
+         // On récupère notre userDb depuis les attributs du servlet context (créé dans le AppContextListener a l'init)
+        UserDb userDb = (UserDb) this.getServletContext().getAttribute("userDb");
 
         // On récupère les paramètres du formulaire
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
+        // on recupère l'instance userDao depuis les attributs du servlet context (créé dans le AppContextListener a l'init)
+        UserDao userDao = (UserDao)this.getServletContext().getAttribute("userDao");
         // On vérifie dans notre base de donnée qu'un user existe avec les identifiants envoyés
-        //User user = userDb.checkUser(email, password);
-        User user = UserDb.getInstance().checkUser(email, password);
+        User user = userDao.findByEmail(email);
 
-        // Si le user est null, les identifiants sont invalides
+        // Si le user est null ou le password incorrect, les identifiants sont invalides
         // On set le message d'erreur et on affiche la page de login
         if (user == null) {
+            request.setAttribute("error", "Veuillez vérifier vos identifiants !");
+            this.getServletContext().getRequestDispatcher("/META-INF/login.jsp").forward(request, response);
+            return; // on arrête la méthode ici
+        }else if(!user.getPassword().equals(password)){
             request.setAttribute("error", "Veuillez vérifier vos identifiants !");
             this.getServletContext().getRequestDispatcher("/META-INF/login.jsp").forward(request, response);
             return; // on arrête la méthode ici
